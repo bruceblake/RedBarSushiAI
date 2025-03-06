3bd1a30da9464f3636f936e00093740965b4fab1
bruceblake <bruceiiiblake@gmail.com>
2025-03-05 21:09:51 -0500

    0

--- celery_app.py
+++ celery_app.py
@@ -13,8 +13,8 @@ def make_celery(app):
 
     celery = Celery(
         "tasks" ,
-        broker="redis://localhost:6379/0",
-        backend="redis://localhost:6379/0"# Use Redis as the message broker
+        broker="redis://127.0.0.1:6379",
+        backend="redis://127.0.0.1:6379"# Use Redis as the message broker
     )
 
     # Update Celery configuration with Flask's config
